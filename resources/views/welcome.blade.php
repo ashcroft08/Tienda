76e7304e5dca7862862b745d705c2a261ce9dac8
23Kevin23 <108309727+23Kevin23@users.noreply.github.com>
2024-06-26 16:12:46 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,14 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-        {{-- Sección Header --}}
-        @include('layouts.nav')
+{{-- Sección Header --}}
+@include('layouts.header')
 
     <div class="container border m-4 p-10">
         <div class="overflow-x-auto">
             <div class="flex space-x-4">
-                <img src="..//" alt="">
+                <img src="..//..//public/img/56f2fa57-5ccb-4db4-82d9-073f88d4a52e.jpg" alt="">
+                <img src="..//..//public/img/a26bcb4e-87b3-4774-98c2-1b0f7ffc2c9f.jpg" alt="">
             </div>
         </div>
     </div>
