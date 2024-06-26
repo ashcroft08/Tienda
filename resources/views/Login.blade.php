78c1900e6be26db4d61a3358f52b118a6ebcb3a4
CalvaDiego <153756604+CalvaDiego@users.noreply.github.com>
2024-06-26 15:47:56 -0500

    Login

---
+++ resources/views/Login.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de Login</title>
+</head>
+<body>
+    <div>
+        <h2>Login</h2>
+        <form action="#" method="POST">
+            <label for="username">Usuario:</label><br>
+            <input type="text" id="username" name="username" required><br><br>
+            
+            <label for="password">Contraseña:</label><br>
+            <input type="password" id="password" name="password" required><br><br>
+            
+            <input type="submit" value="Iniciar sesión">
+        </form>
+    </div>
+</body>
+</html>
