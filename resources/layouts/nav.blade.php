777ea4b8b897531c215f4087ac70ea83781163e2
23Kevin23 <108309727+23Kevin23@users.noreply.github.com>
2024-06-26 16:10:22 -0500

    layouts
    s

---
+++ resources/layouts/nav.blade.php
@@ -0,0 +1,12 @@
+<nav class="bg-teal-500 p-4">
+        <div class="container mx-auto flex justify-between items-center">
+            <a class="border" href="">LOGO</a>
+            <ul class="flex space-x-4">
+                <li class="px-2"><a href="#" class="text-white">INICIO</a></li>
+                <li class="px-2"> <a href="#" class="text-white">COMPRA</a></li>
+                <li class="px-2"> <a href="#" class="text-white">NOSOTROS</a></li>
+                <li class="px-2"> <a href="{{route('indexLogin')}}" class="text-white">LOGIN</a></li>
+                <li class="px-2"> <a href="#" class="text-white">REGISTRARSE</a></li>
+            </ul>    
+        </div>
+    </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,23 +7,13 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <nav class="bg-teal-500 p-4">
-        <div class="container mx-auto flex justify-between items-center">
-            <a class="border" href="">LOGO</a>
-            <ul class="flex space-x-4">
-                <li class="px-2"><a href="#" class="text-white">INICIO</a></li>
-                <li class="px-2"> <a href="#" class="text-white">COMPRA</a></li>
-                <li class="px-2"> <a href="#" class="text-white">NOSOTROS</a></li>
-                <li class="px-2"> <a href="{{route('indexLogin')}}" class="text-white">LOGIN</a></li>
-                <li class="px-2"> <a href="#" class="text-white">REGISTRARSE</a></li>
-            </ul>    
-        </div>
-    </nav>
+        {{-- Sección Header --}}
+        @include('layouts.nav')
 
     <div class="container border m-4 p-10">
         <div class="overflow-x-auto">
             <div class="flex space-x-4">
-                <img src="" alt="">
+                <img src="..//" alt="">
             </div>
         </div>
     </div>
